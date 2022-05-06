8247c872f6089d08545c8fd9bb17743613815a70
Ruben Nijhuis <contact@rubennijhuis.com>
2022-05-06 21:11:36 +0200

    Configuration update

---
+++ scripts/updateconf.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+# Files to copy over
+ZSHRC=~/.zshrc
+VSC=~/Library/Application\ Support/Code/User/settings.json
+
+# Config dir location
+CONFIGFOLDER=~/Code/Config
+# Copy these files over
+cp $ZSHRC $CONFIGFOLDER
+cp $VSC $CONFIGFOLDER
+
+# Push changes to github
+cd $CONFIGFOLDER
+git pull
+git add .
+git commit -m "Configuration files update"
+git push
