52787a23c691862495c1deb7f30735601ba27cfe
Ruben Nijhuis <contact@rubennijhuis.com>
2022-05-15 23:09:36 +0100

    Configuration files update

--- scripts/updateconf.sh
+++ scripts/updateconf.sh
@@ -3,12 +3,16 @@
 # Files to copy over
 ZSHRC=~/.zshrc
 VSC=~/Library/Application\ Support/Code/User/settings.json
+GIT=~/.gitconfig
+CONFSCRIPT=~/Code/Scripts/updateconf.sh
 
 # Config dir location
 CONFIGFOLDER=~/Code/Config
 # Copy these files over
 cp $ZSHRC $CONFIGFOLDER
 cp $VSC $CONFIGFOLDER
+cp $GIT $CONFIGFOLDER
+cp $CONFSCRIPT $CONFIGFOLDER/scripts
 
 # Push changes to github
 cd $CONFIGFOLDER
